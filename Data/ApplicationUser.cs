00fdd2bd5e9e83d3b9040c396d478275c18bd5b1
Mike Cooper <mike.cooper@fiixsoftware.com>
2025-07-09 11:36:48 -0400

    Add project files.

---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Local_WRP.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+}
