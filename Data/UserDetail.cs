8aca7a5f6fe28d31cce9e784a61bb41e218225ef
Mike Cooper <mike.cooper@fiixsoftware.com>
2025-07-31 17:05:47 -0400

    fixed issue with null default language on user first login.

--- Data/UserDetail.cs
+++ Data/UserDetail.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime CreatedDateUTC { get; set; } = DateTime.UtcNow;
-        public string Language { get; set; }
+        public string Language { get; set; } = "en";
     }
 }
